fe77c14f0d7a8a436cddd14a581156e16cc9ad74
Gabaa <7581436+Gabaa@users.noreply.github.com>
2021-10-27 22:10:20 +0200

    Show fixed number of task list items

--- src/tasks/TaskList.js
+++ src/tasks/TaskList.js
@@ -1,15 +1,22 @@
+import { List } from "immutable";
+import _ from "lodash";
+import { useState } from "react";
 import Task from "./Task";
 import './TaskList.css';
 
 function TaskList() {
-  const tasks = ["Brush teeth", "Clean kitchen", "Do dishes"];
+  const TASKS_PER_PAGE = 20;
+  const [tasks, setTasks] = useState(List(["Brush teeth", "Clean kitchen", "Do dishes"]));
 
   return (
     <div className="task-list">
       <h2>Task list</h2>
       <div className="tasks">
-        {tasks.map(task => {
-          return <Task name={task} />
+        {_.range(TASKS_PER_PAGE).map(i => {
+          if (i < tasks.size) {
+            return <Task key={i} name={tasks.get(i)} />
+          }
+          return <Task key={i} name="???" />
         })}
       </div>
     </div>

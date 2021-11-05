dad61ad210ab977dbe5bb68356f875d6abe99d30
Mikkel Erfurth Gaba <mikkelgaba@post.au.dk>
2021-11-05 19:26:22 +0100

    Basic to-do list

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,80 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, h2, input, li, text, ul)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onClick, onInput)
+import List exposing (drop, indexedMap, take)
 
-main = Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement | Reset
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
 
+
+
+-- Model
+
+
+type alias Model =
+    { items : List String }
+
+
+init : Model
+init =
+    { items =
+        [ "hello"
+        , "how are you"
+        , "i am well"
+        , "thanks"
+        ]
+    }
+
+
+
+-- Update
+
+
+type Msg
+    = Update Int String
+    | Remove Int
+    | Add
+
+
+update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Update index item ->
+            { model | items = take index model.items ++ (item :: drop (index + 1) model.items) }
 
-    Decrement ->
-      model - 1
+        Remove i ->
+            { model | items = take i model.items ++ drop (i + 1) model.items }
 
-    Reset ->
-      0
+        Add ->
+            { model | items = model.items ++ [ "" ] }
 
+
+
+-- View
+
+
+renderItem : Int -> String -> Html Msg
+renderItem index item =
+    li [] [ input [ placeholder "Task", value item, onInput (Update index) ] [] ]
+
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    , div [] []
-    , button [ onClick Reset ] [ text "Reset "]
-    ]
+    div []
+        [ h2 []
+            [ text "Hello!" ]
+        , div
+            []
+            [ ul []
+                (indexedMap renderItem model.items
+                    ++ [ li []
+                            [ button [ onClick Add ] [ text "New" ] ]
+                       ]
+                )
+            ]
+        ]

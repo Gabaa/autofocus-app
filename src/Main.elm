b37538383dde60afc1c875de8afef60bcffaba3d
Mikkel Erfurth Gaba <mikkelgaba@post.au.dk>
2021-11-08 17:23:26 +0100

    Show fixed number of tasks

--- src/Main.elm
+++ src/Main.elm
@@ -61,10 +61,15 @@ update msg model =
                     { model | items = Array.set i { task | name = item } model.items }
 
                 Nothing ->
+                    -- TODO: consider updating model to be Array (Maybe Task)
+                    --       so that we can write to objects at any index
+                    --       OR just enforce that you can only write to the
+                    --       index just after the last (then updating array
+                    --       is trivial). Last approach is probably better.
                     model
 
         Remove i ->
-            { model | items = Array.append (Array.slice 0 i model.items) (Array.slice (i + 1) (Array.length model.items) model.items) }
+            { model | items = removeArrayItem i model.items }
 
         Add ->
             { model | items = Array.push (newTask "") model.items }
@@ -78,33 +83,43 @@ update msg model =
                     model
 
 
+removeArrayItem : Int -> Array a -> Array a
+removeArrayItem index items =
+    Array.append
+        (Array.slice 0 index items)
+        (Array.slice (index + 1) (Array.length items) items)
+
+
 
 -- View
 
 
 view : Model -> Html Msg
 view model =
-    let
-        renderItem index item =
-            li []
-                [ div []
-                    [ input [ type_ "checkbox", checked item.done, onCheck (TaskUpdateDone index) ] []
-                    , input [ placeholder "Task", value item.name, disabled item.done, onInput (Update index) ] []
-                    , button [ onClick (Remove index) ] [ text "X" ]
-                    ]
-                ]
-    in
     div []
         [ h2 []
             [ text "Hello!" ]
         , div
             []
-            [ ul []
-                (List.append
-                    (List.indexedMap renderItem (Array.toList model.items))
-                    [ li []
-                        [ button [ onClick Add ] [ text "New" ] ]
-                    ]
-                )
-            ]
+            [ viewTaskList model ]
         ]
+
+
+viewTaskList : Model -> Html Msg
+viewTaskList model =
+    let
+        viewTask index =
+            let
+                task =
+                    Array.get index model.items |> Maybe.withDefault (newTask "")
+            in
+            li
+                []
+                [ div []
+                    [ input [ type_ "checkbox", checked task.done, onCheck (TaskUpdateDone index) ] []
+                    , input [ placeholder "Task", value task.name, disabled task.done, onInput (Update index) ] []
+                    , button [ onClick (Remove index) ] [ text "X" ]
+                    ]
+                ]
+    in
+    ul [] (List.range 0 25 |> List.map viewTask)
